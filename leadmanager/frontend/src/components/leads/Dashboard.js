bd3d3cf003d65d37ee5dac141e4c433973f66262
Sonnek75 <jforr750@gmail.com>
2019-10-19 21:49:59 -0700

    Fourth Commit

---
+++ leadmanager/frontend/src/components/leads/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import Form from './Form.js';
+import Leads from './Leads.js'
+
+const Dashboard = () => {
+    return (
+       <Fragment>
+           <Form />
+           <Leads />
+       </Fragment>
+    );
+}
+
+export default Dashboard;
